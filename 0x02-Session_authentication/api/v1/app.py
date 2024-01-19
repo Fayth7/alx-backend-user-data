55dc178cd9bec7e54a4b059e2a23fd36a772ac14
Fayth7 <flexflakes@gmail.com>
2024-01-19 11:43:03 +0000

    app.py

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -2,80 +2,86 @@
 """
 Route module for the API
 """
-from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS, cross_origin
-import importlib
+from flask_cors import (CORS, cross_origin)
+import os
+from os import getenv
 
 
 app = Flask(__name__)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
+AUTH_TYPE = getenv("AUTH_TYPE")
 
-AUTH_TYPE_MAP = {
-    'basic_auth': 'api.v1.auth.basic_auth.BasicAuth',
-    'session_auth': 'api.v1.auth.session_auth.SessionAuth',
-    'session_exp_auth': 'api.v1.auth.session_exp_auth.SessionExpAuth',
-    'session_db_auth': 'api.v1.auth.session_db_auth.SessionDBAuth',
-    'default_auth': 'api.v1.auth.auth.Auth'
-}
-
-auth_type = getenv("AUTH_TYPE", 'default_auth')
-auth_class = AUTH_TYPE_MAP.get(auth_type, AUTH_TYPE_MAP['default_auth'])
-
-try:
-    auth_module = importlib.import_module(auth_class)
-    auth = getattr(auth_module, auth_class.split('.')[-1])()
-except ImportError:
-    raise ImportError(f"Error importing {auth_class}")
-
-EXCLUDED_PATHS = [
-    '/api/v1/status/',
-    '/api/v1/unauthorized/',
-    '/api/v1/forbidden/',
-    '/api/v1/auth_session/login/'
-]
+if AUTH_TYPE == "auth":
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+elif AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "session_auth":
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
+elif AUTH_TYPE == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif AUTH_TYPE == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
 
 
-def check_authorization(request):
-    """Check if request needs authorization"""
-    if auth and auth.require_auth(request.path, EXCLUDED_PATHS):
-        if auth.authorization_header(request) is None and auth.session_cookie(
-                request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-        request.current_user = auth.current_user(request)
-
-
-@app.before_request
-def request_filter() -> None:
-    """Filter requests"""
-    check_authorization(request)
-
-
-@app.errorhandler(404)
+@ app.errorhandler(404)
 def not_found(error) -> str:
-    """Not found handler"""
+    """ Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
 
 
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Unauthorized handler"""
+@ app.errorhandler(401)
+def unauthorized_error(error) -> str:
+    """ Unauthorized handler
+    """
     return jsonify({"error": "Unauthorized"}), 401
 
 
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Forbidden handler"""
+@ app.errorhandler(403)
+def forbidden_error(error) -> str:
+    """ Forbidden handler
+    """
     return jsonify({"error": "Forbidden"}), 403
+
+
+@ app.before_request
+def before_request() -> str:
+    """ Before Request Handler
+    Requests Validation
+    """
+    if auth is None:
+        return
+
+    excluded_paths = ['/api/v1/status/',
+                      '/api/v1/unauthorized/',
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
+
+    if not auth.require_auth(request.path, excluded_paths):
+        return
+
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
+        abort(401)
+
+    current_user = auth.current_user(request)
+    if current_user is None:
+        abort(403)
+
+    request.current_user = current_user
 
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", "5000"))
+    port = getenv("API_PORT", "5000")
     app.run(host=host, port=port)
